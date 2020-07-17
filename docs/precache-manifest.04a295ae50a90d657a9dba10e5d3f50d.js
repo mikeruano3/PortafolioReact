d57555fbee80f8f4683ff3b4d2dfc89816f68db7
Miguel Ruano <miguelruano35@gmail.com>
2020-07-17 00:23:54 -0600

    Agregar docs

---
+++ docs/precache-manifest.04a295ae50a90d657a9dba10e5d3f50d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df236955407bec7b72168ed6e7a0e84e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b4795052bae95a5c1e",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "2510d0e4dc09bfd4e055",
+    "url": "/static/css/main.c6b619c9.chunk.css"
+  },
+  {
+    "revision": "10b4795052bae95a5c1e",
+    "url": "/static/js/2.b88976fc.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.b88976fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2510d0e4dc09bfd4e055",
+    "url": "/static/js/main.0aac8c38.chunk.js"
+  },
+  {
+    "revision": "35b14380c327903f0dd6",
+    "url": "/static/js/runtime-main.d904f94f.js"
+  },
+  {
+    "revision": "6d65f1955c980cf4bb032529ece9b4d3",
+    "url": "/static/media/profile.6d65f195.jpeg"
+  },
+  {
+    "revision": "cba0b89d2bf2d96a1ed26edb5849f804",
+    "url": "/static/media/react-logo.cba0b89d.png"
+  }
+]);
